d14f291dcb90a5fcdb76ef4d4b7d0c1e564edc4d
Minh Pham <mpham@naver.com>
2018-12-05 09:27:05 +0100

    Work on Bar Style

--- style.js
+++ style.js
@@ -14,28 +14,23 @@ $(document).ready(function(){
       width:$(this).attr('data-percent')
     },6000);
   });
+ $('.skillbar').each(function(){
+    $(this).find('.skillbar-bar-percent').animate({
+      width:$(this).attr('.Count')
+    },6000);
+  });
 });
 
-// $('.Count').each(function () {
-//   var $this = $(this);
-//   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-//     duration: 6000,
-//     easing: 'swing',
-//     step: function () {
-//       $this.text(Math.ceil(this.Counter));
-//     }
-//   });
-// });
 
-
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+$('.Count').each(function () {
+  var $this = $(this);
+  $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 6000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil($this.Counter));
+    }
+  });
 });
+
+
