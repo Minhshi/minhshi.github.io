854f2acade807f197386ab65d607efb4b8aa83a3
Minh Pham <mpham@naver.com>
2018-12-06 07:46:37 +0100

    Add Style to Bars

--- style.js
+++ style.js
@@ -1,12 +1,9 @@
 $(function() {
-  $('.btn').on('click', function(e) {
+  $('.see-more').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $('.container2').offset().top}, 500, 'linear');
   });
 });
-
-
-
 
 $(document).ready(function(){
   $('.skillbar').each(function(){
@@ -14,23 +11,19 @@ $(document).ready(function(){
       width:$(this).attr('data-percent')
     },6000);
   });
- $('.skillbar').each(function(){
-    $(this).find('.skillbar-bar-percent').animate({
-      width:$(this).attr('.Count')
-    },6000);
-  });
 });
 
-
+$(document).ready(function(){
 $('.Count').each(function () {
-  var $this = $(this);
-  $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-    duration: 6000,
-    easing: 'swing',
-    step: function () {
-      $this.text(Math.ceil($this.Counter));
-    }
-  });
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 6000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
 });
-
+});
 
